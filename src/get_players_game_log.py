3e6fbeab15c89829295ff027d236efcffbec6db9
ericbackman <eric.backman@sympatico.ca>
2023-11-17 18:08:17 -0500

    rework how i get player codes, delete cleaning, working download all player logs in games post 1983 (players whos careers ended after 2000

--- src/get_players_game_log.py
+++ src/get_players_game_log.py
@@ -3,36 +3,47 @@ from util.player_logs_helper import get_and_merge_all_logs
 
 import logging
 import sys
+import os
+
 logging.basicConfig(filename='raw_data/logging.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
-
-
 
 # Planning
 #
 # This file will need to do:
-# TODO import post 2000s player csv and create looping logic
-# TODO for each player and for each season, pull basic and advanced game stats
-# TODO take cleaned df's and merge
-# TODO append to larger df, write to CSV, go by last name letter to start? end up with 2
-
+# TODO import players who did not play in the 2000s
+# TODO add player names to rows, repetitive but needed for searching
+# TODO create pre col conversion check to convert np.nan to 0 for int cols
 
 if __name__ == "__main__":
-    nba_players = pd.read_csv('raw_data/players_active_in_2000s.csv')
-    df_master = pd.DataFrame()
+    nba_players = pd.read_csv('raw_data/nba_player_codes_v2.csv')
+    total_entries = nba_players.shape[0]
 
+    directory_path = f"{os.getcwd()}/raw_data/player_career_game_logs"
+    files = [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
     # Rough looping example
-    for index, row in nba_players.iterrows():
+    for idx, row in nba_players.iterrows():
         first = row['first_name']
         last = row['last_name']
+        if first == "Terry" and last == "Cummings":
+            print("skipping terry because he missing info")
+            continue
         start_year = int(row['From'])
         end_year = int(row['To'])
-        for season in range(start_year, end_year+1):
-            print(first, last, season)
-            df = get_and_merge_all_logs(first, last, season)
-            df_master = pd.concat([df_master, df])
-
-
-
-
-
+        filename = f"{first}_{last}_game_logs.csv"
+        if filename in files:
+            print(f"Skipping {first} {last}. CSV exists")
+            continue
+        if start_year >= 2000 or end_year >= 2000:
+            df_master = pd.DataFrame()
+            print(f"Collecting {first} {last}, played {start_year} to {end_year}")
+            for season in range(start_year, end_year + 1):
+                df = get_and_merge_all_logs(season, row['bbref'])
+                df_master = pd.concat([df_master, df])
+            if not df_master.empty:
+                df_master['first_name'] = first
+                df_master['last_name'] = last
+                df_master.to_csv(f"{directory_path}/{filename}")
+        else:
+            print(f"Skipping {first} {last} played from {start_year} to {end_year}")
+        print(f"{100*(idx/total_entries)}")
--- src/util/player_logs_helper.py
+++ src/util/player_logs_helper.py
@@ -1,41 +1,50 @@
 import bs4
 import pandas as pd
+import numpy as np
+import time
 from src.util.selenium_helper import get_webpage_html
 from src.util.util_dicts import home_map, player_games_log_col_order
-import logging
-logging.basicConfig(filename='logging.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
 
 
-
-def player_name_to_bbref_code(first, last):
-    if len(last) >= 5:
-        return "{}/{}{}01".format(last[:1], last[:5], first[:2]).lower()
-    else:
-        return "{}/{}{}01".format(last[:1], last, first[:2]).lower()
+rate_limited = "We apologize, but you have triggered rate limiting by our cloud service provider."
+not_found = "We apologize, but this page could not be found."
 
 
-def get_player_season_games_log(first, last, season):
-    player_code = player_name_to_bbref_code(first, last)
-    url = f"https://www.basketball-reference.com/players/{player_code}/gamelog/{season}"
+def get_player_season_games_log(season, code):
+    url = f"https://www.basketball-reference.com/players/{code}/gamelog/{season}"
     html = get_webpage_html(url)
     soup = bs4.BeautifulSoup(html, 'html.parser')
-    table = soup.find(lambda tag: tag.name == 'table' and tag.has_attr('id') and tag['id'] == 'pgl_basic')
-    df = pd.read_html(str(table))[0]
+    try:
+        table = soup.find(lambda tag: tag.name == 'table' and tag.has_attr('id') and tag['id'] == 'pgl_basic')
+        df = pd.read_html(str(table))[0]
+    except ValueError:
+        print(f"Unable to find {season} for {code}")
+        df = None
+        if rate_limited in soup.body.findAll(string=rate_limited): # When we hit the rate limited check
+            for i in range(1,62):
+                print(f"Sleeping {i}")
+                time.sleep(60)
+        if not_found in soup.body.findAll(string=not_found):
+            print("-----------------------------------------------------------------------")
+            print(f"404 error for {code} during {season}")
+            print("-----------------------------------------------------------------------")
     return df
 
 
 # Specific for the player game log table
 # TODO, investigate try/catch for this in case missing values?
+# Specific for the player game log table
+# TODO, investigate try/catch for this in case missing values?
 def column_type_conversion(df):
-    df.loc[:, 'tm_game_num'] = df['tm_game_num'].astype(int)
-    df.loc[:, 'p_game_num'] = df['p_game_num'].astype(int)
+    df.loc[:, 'tm_game_num'] = df['tm_game_num'].astype(float)
+    df.loc[:, 'p_game_num'] = df['p_game_num'].astype(float)
     df.loc[:, 'home'] = df['home'].astype(bool)
     df.loc[:, 'GS'] = df['GS'].astype(bool)
     df.loc[:, 'FG'] = df['FG'].astype(int)
     df.loc[:, 'FGA'] = df['FGA'].astype(int)
     df.loc[:, 'FG%'] = df['FG%'].astype(float)
     df.loc[:, '3P'] = df['3P'].astype(int)
-    df.loc[:, '3PA'] = df['3PA'].astype(int)
+    df.loc[:, '3PA'] = df['3PA'].astype(float)
     df.loc[:, '3P%'] = df['3P%'].astype(float)
     df.loc[:, 'FT'] = df['FT'].astype(int)
     df.loc[:, 'FTA'] = df['FTA'].astype(int)
@@ -54,7 +63,7 @@ def column_type_conversion(df):
     df.loc[:, 'age_yrs'] = df['age_yrs'].astype(int)
     df.loc[:, 'age_days'] = df['age_days'].astype(int)
     df.loc[:, 'score_diff'] = df['score_diff'].astype(int)
-    if type(df.date[1]) == str:
+    if type(df.iloc[0]['date']) == str:
         df.loc[:, 'date'] = pd.to_datetime(df['date'], format='mixed')
     return df
 
@@ -63,17 +72,21 @@ def extract_date(dt):  # To create gameID column
     return dt.date().strftime("%Y%m%d")
 
 
-def clean_player_games_log(df):
+def clean_player_games_log(df, season):
     df = df[df['G'] != 'G'].copy()  # remove formatting rows
     column_rename = {'Rk': 'tm_game_num', 'G': 'p_game_num', 'Date': 'date', 'Age': 'age', 'Unnamed: 5': 'home',
                      'Unnamed: 7': 'result', '+/-': 'PM'}
     df.rename(columns=column_rename, inplace=True)  # Rename necessary columns
 
     df = df[df['GS'] != 'Inactive'].copy()  # Remove rows where the player was inactive for the game
-    df = df[df[
-                'GS'] != 'Did Not Dress'].copy()  # Remove rows where player was not active (for player stats research would want to keep, but for ML not needed
+    df = df[df['GS'] != 'Did Not Dress'].copy()  # Remove rows where player was not active (for player stats research would want to keep, but for ML not needed
     df = df[df['GS'] != 'Did Not Play'].copy()  # Remove rows where player was not active, mutual decision? lol
     df = df[df['GS'] != 'Not With Team'].copy()  # Remove rows where player was not with team?
+    df = df[df['GS'] != 'Player Suspended'].copy()  # Remove rows where player was suspended and did not play
+
+    if df.shape[0] == 0:
+        print("No active games for given season")
+        return None
 
     df.loc[:, 'home'] = df['home'].replace(home_map)  # Map home to a bool column, true = home
 
@@ -82,6 +95,9 @@ def clean_player_games_log(df):
     df[['result', 'score_diff']] = df['result'].str.split('(', expand=True)
     df['score_diff'] = df['score_diff'].str.rstrip(')')
     df = df.drop(columns=['age'])
+
+    if season < 1997:
+        df['PM'] = np.nan
 
     df = column_type_conversion(df)  # Convert all columns to required type
     df['gameID'] = df['date'].apply(extract_date) + df["Tm"] + df["Opp"]  # Create gameID column
@@ -92,9 +108,11 @@ def clean_player_games_log(df):
 
 # Specific for the player game log table
 # TODO, investigate try/catch for this in case missing values?
+# Specific for the player game log table
+# TODO, investigate try/catch for this in case missing values?
 def adv_column_type_conversion(df):
-    df.loc[:, 'tm_game_num'] = df['tm_game_num'].astype(int)
-    df.loc[:, 'p_game_num'] = df['p_game_num'].astype(int)
+    df.loc[:, 'tm_game_num'] = df['tm_game_num'].astype(float)
+    df.loc[:, 'p_game_num'] = df['p_game_num'].astype(float)
     df.loc[:, 'home'] = df['home'].astype(bool)
     df.loc[:, 'GS'] = df['GS'].astype(bool)
     df.loc[:, 'TS%'] = df['TS%'].astype(float)
@@ -107,34 +125,32 @@ def adv_column_type_conversion(df):
     df.loc[:, 'BLK%'] = df['BLK%'].astype(float)
     df.loc[:, 'TOV%'] = df['TOV%'].astype(float)
     df.loc[:, 'USG%'] = df['USG%'].astype(float)
-    df.loc[:, 'ORtg'] = df['ORtg'].astype(int)
-    df.loc[:, 'DRtg'] = df['DRtg'].astype(int)
+    df.loc[:, 'ORtg'] = df['ORtg'].astype(float)
+    df.loc[:, 'DRtg'] = df['DRtg'].astype(float)
     df.loc[:, 'GmSc'] = df['GmSc'].astype(float)
     df.loc[:, 'BPM'] = df['BPM'].astype(float)
     df.loc[:, 'age_yrs'] = df['age_yrs'].astype(int)
     df.loc[:, 'age_days'] = df['age_days'].astype(int)
     df.loc[:, 'score_diff'] = df['score_diff'].astype(int)
-    if type(df.date[1]) == str:
+    if type(df.iloc[0]['date']) == str:
         df.loc[:, 'date'] = pd.to_datetime(df['date'], format='mixed')
     return df
 
 
-def get_player_adv_season_games_log(first, last, season):
-    player_code = player_name_to_bbref_code(first, last)
-    url = f"https://www.basketball-reference.com/players/{player_code}/gamelog-advanced/{season}"
+def get_player_adv_season_games_log(season, code):
+    url = f"https://www.basketball-reference.com/players/{code}/gamelog-advanced/{season}"
     html = get_webpage_html(url)
     soup = bs4.BeautifulSoup(html, 'html.parser')
-    table = soup.find(lambda tag: tag.name == 'table' and tag.has_attr('id') and tag['id'] == 'pgl_advanced')
     try:
+        table = soup.find(lambda tag: tag.name == 'table' and tag.has_attr('id') and tag['id'] == 'pgl_advanced')
         df = pd.read_html(str(table))[0]
     except ValueError:
-        logging.warning(f"No table found for player {first} {last} in {season}")
-    finally:
+        print(f"Unable to find {season} for {code}")
         df = None
     return df
 
 
-def clean_player_adv_games_log(df):
+def clean_player_adv_games_log(df, season):
     col_order = ['gameID', 'tm_game_num', 'p_game_num', 'date', 'age_yrs', 'age_days', 'Tm', 'home', 'Opp',
                  'result', 'score_diff', 'GS', 'MP', 'TS%', 'eFG%', 'ORB%', 'DRB%', 'TRB%', 'AST%',
                  'STL%', 'BLK%', 'TOV%', 'USG%', 'ORtg', 'DRtg', 'GmSc', 'BPM']
@@ -148,6 +164,8 @@ def clean_player_adv_games_log(df):
     df = df[df['GS'] != 'Did Not Dress'].copy()  # Remove rows where player was not active (for player stats research would want to keep, but for ML not needed
     df = df[df['GS'] != 'Did Not Play'].copy()  # Remove rows where player was not active, mutual decision? lol
     df = df[df['GS'] != 'Not With Team'].copy()  # Remove rows where player was not with team?
+    df = df[df['GS'] != 'Player Suspended'].copy()  # Remove rows where player was suspended and did not play
+
 
     # NOTE: Will be more of these to drop, e.g. did not dress
     df.loc[:, 'home'] = df['home'].replace(home_map)  # Map home to a bool column, true = home
@@ -158,6 +176,9 @@ def clean_player_adv_games_log(df):
     df['score_diff'] = df['score_diff'].str.rstrip(')')
 
     df = df.drop(columns=['age'])
+
+    if season < 1985:
+        df['BPM'] = np.nan
 
     df = adv_column_type_conversion(df)
 
@@ -175,11 +196,16 @@ def merge_gamelogs(df, df_adv):
     return merged_df
 
 
-def get_and_merge_all_logs(first, last, season):
-    df_adv = get_player_adv_season_games_log(first, last, season)
-    if df_adv:
-        df_adv = clean_player_adv_games_log(df_adv)
-    df = get_player_season_games_log(first, last, season)
-    df = clean_player_games_log(df)
+def get_and_merge_all_logs(season, code):
+    print(season)
+    df = get_player_season_games_log(season, code)
+    if df is None:
+        return None
+    df = clean_player_games_log(df, season)
+    if df is None:
+        return None
+    df_adv = get_player_adv_season_games_log(season, code)
+    if df_adv is None:
+        return None
+    df_adv = clean_player_adv_games_log(df_adv, season)
     return merge_gamelogs(df, df_adv)
-
